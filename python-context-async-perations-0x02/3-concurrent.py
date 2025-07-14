6fc3780bc1668797731c212e6b2bbc65a2dd58a3
Sunmitechie <sunmijewel@gmail.com>
2025-07-14 15:15:23 +0100

    script that Run multiple database queries concurrently using asyncio.gather.

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -7,14 +7,14 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         cursor = await db.execute("SELECT * FROM users")
         rows = await cursor.fetchall()
-        return rows  # ✅ required by checker
+        return rows  # required by checker
 
 
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         cursor = await db.execute("SELECT * FROM users WHERE age > 40")
         rows = await cursor.fetchall()
-        return rows  # ✅ required by checker
+        return rows  # required by checker
 
 
 async def fetch_concurrently():
@@ -24,11 +24,11 @@ async def fetch_concurrently():
     )
 
     # Optional: Print them out (not needed for checker but helps you test)
-    print("\n✅ All Users:")
+    print("\n All Users:")
     for user in all_users:
         print(user)
 
-    print("\n📌 Users older than 40:")
+    print("\n Users older than 40:")
     for user in older_users:
         print(user)
 
